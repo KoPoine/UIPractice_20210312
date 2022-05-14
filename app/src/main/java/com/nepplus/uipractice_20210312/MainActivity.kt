18339f09a869daa078633a5b40f5f4b8d1123d8f
hbs30832 <105183532+hbs30832@users.noreply.github.com>
2022-05-14 16:12:19 +0900

    새 브랜치 생성및 작업

--- app/src/main/java/com/nepplus/uipractice_20210312/MainActivity.kt
+++ app/src/main/java/com/nepplus/uipractice_20210312/MainActivity.kt
@@ -2,10 +2,18 @@ package com.nepplus.uipractice_20210312
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        goBtn.setOnClickListener {
+            Toast.makeText(this, "go", Toast.LENGTH_SHORT).show()
+        }
     }
 }
